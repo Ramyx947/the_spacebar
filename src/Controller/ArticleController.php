756a83050f4bf286e1ae1301dc7147fb4855262a
Ramona <r1mari@yahoo.com>
2019-02-03 18:09:14 +0000

    twig templates

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-class ArticleController
+
+
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -19,9 +22,17 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show the article: %s',
-        $slug
-        ));
+        $comments = [
+            "Peter Piper picked a peck of pickled peppers.
+            A peck of pickled peppers Peter Piper picked.
+            If Peter Piper picked a peck of pickled peppers?
+            Where's the peck of pickled peppers Peter Piper picked?",
+            "Nine nimble noblemen nibbling nuts.",
+            "Ingenious iguanas improvising an intricate impromptu on impossibly-impractical instruments."
+        ];
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments, 
+        ]);
     }
 }
